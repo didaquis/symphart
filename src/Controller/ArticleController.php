41e107462731ac98ef1c92f23864caf0888f2066
Dídac García <didaquis@gmail.com>
2019-09-27 00:09:04 +0200

    Added Annotations component

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,8 +3,14 @@
 	namespace App\Controller;
 
 	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Component\Routing\Annotation\Route;
+	use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 	class ArticleController {
+		/**
+		 * @Route("/")
+		 * @Method({"GET"})
+		 */
 		public function index() {
 			return new Response('<html><body><h1>It Works</h1></body></html>');
 		}
